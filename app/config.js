22cb9cf5cbcc8ee8c81a73184dca940ce6d08221
ktsosno <ktsosno@gmail.com>
2017-02-18 13:48:04 -0800

    Adjust server for running as root

---
+++ app/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PORT: 9191,
+  USER: 'kyle'
+}
--- app/server.js
+++ app/server.js
@@ -10,21 +10,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const username = require('username');
 const argv = require('minimist')(process.argv.slice(2));
+
+// These context paths change on user (?), set for root
 const appRouter = require('./app/router');
+const appConfig = require('./app/config');
 
 const app = express();
 const router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/api', router);
+app.use('/', router);
 
 // Register application routes
 appRouter(router);
 
 username().then(username => {
-  const serverPort = argv.p || 8080;
-  const serverUser = argv.u || username;
+  const serverPort = argv.p || appConfig.PORT;
+  const serverUser = argv.u || appConfig.USER;
   const serverIP = argv.i || 'localhost';
 
   app.listen(serverPort, serverIP);
