21fc11b136292dfa6cb629d081b14a6d3b1b3673
ktsosno <ktsosno@gmail.com>
2017-02-18 15:04:04 -0800

    Adjust load API to properly parse job

--- app/routes/load.js
+++ app/routes/load.js
@@ -1,3 +1,5 @@
+const later = require('later');
+
 /**
   * Directive for loading all crons
   */
@@ -7,8 +9,29 @@ module.exports = (tab, response) => {
 
   if (jobs.length > 0) {
     jobs.forEach((job) => {
-      // TODO: More logical parsing of this
-      parsedJobs.push(job.toString());
+      // We can be smarter about this
+      let jobArr = job.toString().split(' ');
+      let timingArr = jobArr.slice(0,5);
+      let timingStr = timingArr.join(' ');
+      let action = jobArr.slice(5).join(' ');
+
+      // Construct more useable job object
+      const parsedJob = {
+        action,
+        timing: {
+          fullString: timingStr,
+          values: {
+            dayOfWeek: timingArr[0],
+            month: timingArr[1],
+            dayOfMonth: timingArr[2],
+            hour: timingArr[3],
+            minute: timingArr[4],
+            second: timingArr[5]
+          }
+        }
+      };
+
+      parsedJobs.push(parsedJob);
     });
   }
 
