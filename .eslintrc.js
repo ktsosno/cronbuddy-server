0aaaddc25a9d620c44430a597e2b1616640fb64a
ktsosno <ktsosno@gmail.com>
2017-02-22 23:54:03 -0800

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ const airbnb = require('eslint-config-airbnb');
 module.exports = {
     "extends": "airbnb",
     "rules": {
-       "import/no-unresolved": 0	
+       "import/no-extraneous-dependencies": 0,
+       "import/no-unresolved": 0,
+       "global-require": 0
     }
 };
